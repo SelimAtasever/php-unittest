59fe2bac233de93ba5eb0ffa6774cf4cafcab8dc
Mustafa Sabri KARAMAN <sabri@kant.ist>
2020-07-06 15:05:20 +0300

    Update PersonelTest.php

--- PersonelTest.php
+++ PersonelTest.php
@@ -33,12 +33,14 @@ class SelimTest extends TestCase {
             true,
             'firstname', 
             'doe', 
-            '11223344556', 
+            '11223344556',
+            'male',
             '0049224591432', 
             'john-doe@mail.com', 
-            null,
+            null, 
             null
        );
+
    }
 
 
@@ -47,7 +49,10 @@ class SelimTest extends TestCase {
 		$this->expectException(PersonnelEmailLengthException::class);
 
 		try{
-		new Personnel(new PersonnelId(1), new RoleId(1), true, 'john', 'doe', '11223344556', '0049224591432', str_repeat('a', 60) . '@mail.com', null, null);	
+			$personnel = $this->validPersonnelWithId(1);
+
+			$personnel->changeEmail(str_repeat('a', 60) . '@mail.com');
+			// new Personnel(new PersonnelId(1), new RoleId(1), true, 'john', 'doe', '11223344556', 'female', '0049224591432', str_repeat('a', 60) . '@mail.com', null, null);	
 		}
 
 		catch(ExceptionCollection $z) {
@@ -62,22 +67,29 @@ class SelimTest extends TestCase {
 
 
 		try {
-		new Personnel(new PersonnelId(1), new RoleId(1), true, 'john', 'doe', '11223344556', '0049224591432', 'jo@ma.st', null, null);	
+
+		$personnel = $this->validPersonnelWithId(1);
+		$personnel->changeEmail('aa@o.com');
+		
+		//new Personnel(new PersonnelId(1), new RoleId(1), true, 'john', 'doe', '11223344556', '0049224591432', 'jo@ma.st', null, null);	
 		}
 
 		catch(ExceptionCollection $w) {
 
 			$this->throwFromExceptionCollection($w, PersonnelEmailLengthException::class);
 		}
-	} 
+	}
 
 
-	public function testLastNameCantComtaimSpeacialCharacters(){
+	public function testLastNameCantContainSpeacialCharacters() {
 
 		$this->expectException(PersonnelLastnameForbiddenCharacterException::class);
 
 		try{
-		new Personnel(new PersonnelId(1), new RoleId(1),true,'john','doe@','11223344556','0049224591432','john-doe@mail.com',null,null);
+		
+		$personnel = $this->validPersonnelWithId(2);
+		$personnel->changeLastname('do!e');
+
 		}
 
 		catch(ExceptionCollection $w) {
@@ -92,7 +104,10 @@ class SelimTest extends TestCase {
 
 		try {
 
-		new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '' ,'0049224591432' , 'john-doe@mail.com', null,null);
+		$personel = $this->validPersonnelWithId(1);
+		$personel->changeTcno('');
+
+		//new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '' ,'0049224591432' , 'john-doe@mail.com', null,null);
 		}
 		catch (ExceptionCollection $e){
 
@@ -103,12 +118,15 @@ class SelimTest extends TestCase {
 	}
 
 
-	public function testTcNoCantBeNanCharacter (){
+	public function testTcNoCantBeNanCharacter () {
 
 		$this->expectException(PersonnelTcnoNANCharacterException::class);
 
 		try{
-		new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '11223344k56' ,'0049224591432' , 'john-doe@mail.com', null,null);
+		
+		$personel = $this->validPersonnelWithId(1);
+		$personel->changeTcno('11223344z56');
+
 		}
 
 		catch(ExceptionCollection $e){
@@ -119,10 +137,14 @@ class SelimTest extends TestCase {
 
 	public function testLastNameCanBeChanged() {
 
-		$personel = new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '11223344556' ,'0049224591432' , 'john-doe@mail.com', null,null);
-		$this->assertEquals('doe' , $personel->getLastname());
-		$personel->changeLastname('Joe');
-		$this->assertEquals('Joe', $personel->getLastname());
+		// $personel = new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '11223344556' ,'0049224591432' , 'john-doe@mail.com', null,null);
+		// $this->assertEquals('doe' , $personel->getLastname());
+		// $personel->changeLastname('Joe');
+		// $this->assertEquals('Joe', $personel->getLastname());
+
+		$personel = $this->validPersonnelWithId(1);
+		$personel->changeLastname('cloe');
+		$this->assertEquals('cloe', $personel->getLastname());
 
 	}
 
@@ -133,7 +155,11 @@ class SelimTest extends TestCase {
 
 		try{
 
-		new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '11223344556' ,'03032', 'john-doe@mail.com', null,null);
+		$personel = $this->validPersonnelWithId(1);
+		$personel->changePhone('04312');
+
+
+		//new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '11223344556' ,'03032', 'john-doe@mail.com', null,null);
 		}
 		catch(ExceptionCollection $e){
 
@@ -149,7 +175,10 @@ class SelimTest extends TestCase {
 
 		try{
 
-		new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '11223344556' , str_repeat(12, 13), 'john-doe@mail.com', null,null);
+		$personel = $this->validPersonnelWithId(2);
+		$personel->changePhone(str_repeat(10, 13));
+
+		//new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '11223344556' , str_repeat(12, 13), 'john-doe@mail.com', null,null);
 		}
 
 		catch(ExceptionCollection $e){
@@ -159,28 +188,47 @@ class SelimTest extends TestCase {
 
 	}
 
-	// public function testPhoneNumberCanChange() {
+	public function testPhoneNumberCanChange() {
 
-	// 	$personel = new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '11223344556' ,'0049224591432' , 'john-doe@mail.com', null,null);
-	// 	$personel->changePhone('533 388 6868');
-	// 	$this->assertEquals('533 388 6868', $personel->getPhone());
+		$personel = $this->validPersonnelWithId(1);
+		$personel->changePhone('533 388 6868');
+		$this->assertEquals('533 388 6868', $personel->getPhone());
 
-	// }
+	}
 
 	public function testFirstNameCanChange() {
 
-		$personel = new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '11223344556' ,'0049224591452', 'john-doe@mail.com', null,null);
-		$personel->changeFirstname('ali');
-		$this->assertEquals('ali', $personel->getFirstname());
+	$personel = $this->validPersonnelWithId(1);
+	$personel->changeFirstname('joe');
+	$this->assertEquals('joe', $personel->getFirstname());
+
 
 	}
+
+	public function testPhoneCantContainLetters(){
+
+		$this->ExceptionCollection(PersonnelPhoneForbiddenCharacterException::class);
+
+		try {
+		$personel = $this->validPersonnelWithId(1);
+		$personel->changePhone('004x9224591432');
+		}
+		catch(ExceptionCollection $p){
+			$this->throwFromExceptionCollection($p, PersonnelPhoneForbiddenCharacterException::class);
+		}
+	}
+
 
 	public function testTcNoMustBe11Characters() {
 
 		$this->expectException(PersonnelTcnoLengthException::class);
 
 		try {
-		$personel = new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '1122334455','0049224591452', 'john-doe@mail.com', null,null);
+		
+		$personal = $this->validPersonnelWithId(1);
+		$personal->changeTcno('1122334455');
+
+		//$personel = new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '1122334455','0049224591452', 'john-doe@mail.com', null,null);
 		}
 		catch(ExceptionCollection $e){
 
@@ -196,7 +244,11 @@ class SelimTest extends TestCase {
 		$this->expectException(PersonnelEmailFormatException::class);
 
 		try {
-		$personel = new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '1122334455','0049224591452', 'john@doe@mail.com', null,null);
+		
+			$personel = $this->validPersonnelWithId(1);
+			$personel->changeEmail('con@doe@mail.com');
+
+		//$personel = new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '1122334455','0049224591452', 'john@doe@mail.com', null,null);
 	  	}
 	  	catch (ExceptionCollection $e) {
 	  		$this->throwFromExceptionCollection($e, PersonnelEmailFormatException::class);
@@ -209,7 +261,10 @@ class SelimTest extends TestCase {
 		$this->expectException(PersonnelPhoneIsNullException::class);
 
 		try {
-			$personel = new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '11223344556', '' , 'john-doe@mail.com', null,null);
+			
+			$personnel = $this->validPersonnelWithId(1);
+			$personnel->changePhone('');
+			//$personel = new Personnel(new PersonnelId(1), new RoleId(1), true ,'john','doe', '11223344556', '' , 'john-doe@mail.com', null,null);
 		}
 		catch (ExceptionCollection $e) {
 			$this->throwFromExceptionCollection($e, PersonnelPhoneIsNullException::class);
