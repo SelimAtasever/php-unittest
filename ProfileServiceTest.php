874b41bc81c08768d08305203e296a057f46c5a6
SelimAtasever <selim.atasever@kant.ist>
2020-09-09 12:47:21 +0300

    Update ProfileServiceTest.php

--- ProfileServiceTest.php
+++ ProfileServiceTest.php
@@ -11,7 +11,10 @@ use \model\IdentityAndAccess\domain\model\IRoleRepository;
 use \model\IdentityAndAccess\application\DTO\PersonnelDTO;
 use \model\IdentityAndAccess\domain\model\Role;
 use \model\IdentityAndAccess\application\DTO\RoleDTO;
-
+use \model\IdentityAndAccess\domain\model\IDepartmentRepository;
+use \model\IdentityAndAccess\domain\model\Department;
+use \model\IdentityAndAccess\domain\model\DepartmentId;
+use \model\IdentityAndAccess\application\IImageDirectAccessLinkProvider;
 
 use PHPUnit\Framework\TestCase;
 
@@ -28,8 +31,8 @@ class ProfileServiceTest extends TestCase {
 		$mock_identity = $this->createMock(IIdentityProvider::class);
 		$mock_identity->method('identity')->willReturn(15);
 
-		$personnel = new Personnel(new PersonnelId(15), new RoleId(1), true, null, 'jon', 'snow', '11223344556', 'male', '0049224591432', 'jon-snow@mail.com', null, null);
-		$this->personnel_dto_correct = PersonnelDTO::fromPersonnel($personnel); 
+		$personnel = new Personnel(new PersonnelId(15), new RoleId(1),null, true, null, 'jon', 'snow', '11223344556', 'male', '0049224591432', 'jon-snow@mail.com', null, null);
+		$this->personnel_dto_correct = PersonnelDTO::fromPersonnel($personnel,null); 
 
 		$stub_repo = $this->createStub(IPersonnelRepository::class);
 		$stub_repo->expects($this->any())->method('findById')->will(
@@ -67,9 +70,16 @@ class ProfileServiceTest extends TestCase {
 			}
 		));
 
+		$department = new Department(new DepartmentId(1), 'name', null, null, 1,1,1,1);
+
+		$mock_dep = $this->createMock(IDepartmentRepository::class);
+		$mock_dep->method('findDepartment')->willReturn($department);
+
+		$image_access_link_provider = $this->createMock(IImageDirectAccessLinkProvider::class);
+		$image_access_link_provider->method('getLink')->willReturn('path as string.....');
 		
 
-	$this->profile_service = new ProfileService($stub_repo, $stub_role, $mock_identity);
+	$this->profile_service = new ProfileService($stub_repo, $stub_role, $mock_dep ,$mock_identity, $image_access_link_provider);
 
 	}
 
