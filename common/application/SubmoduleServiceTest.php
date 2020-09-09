fb9552f23b9b54dcf2689bb69792327f6b87c589
SelimAtasever <selim.atasever@kant.ist>
2020-09-09 16:32:38 +0300

    submodule-service-exceptions-added

--- common/application/SubmoduleServiceTest.php
+++ common/application/SubmoduleServiceTest.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use \model\common\application\SubmoduleService;
 use \model\common\domain\model\IModuleRepository;
 use \model\common\domain\model\ISubmoduleRepository;
@@ -11,6 +10,8 @@ use \model\common\domain\model\Module;
 use \model\common\application\DTO\SubmoduleDTO;
 use \model\common\application\DTO\ModuleDTO;
 
+use model\common\application\exception\SubmoduleNotFoundException;
+use model\common\application\exception\ModuleNotFoundException;
 
 use PHPUnit\Framework\TestCase;
 
@@ -18,27 +19,33 @@ use PHPUnit\Framework\TestCase;
 class SubmoduleServiceTest extends TestCase {
 
 	private SubmoduleService $submodule_service;
-	private Submodule $submodule;
-	private Module $module;
+	private ISubmoduleRepository $submodule_repository;
+	private IModuleRepository $module_repository;
 	
 	protected function setUp() : void {
 
-		$this->submodule = new Submodule(new SubmoduleId(1), new ModuleId(1), 'submodule_1');
-		$this->module = new Module(new ModuleId(1), 'module_1');
+		$submodule = new Submodule(new SubmoduleId(1), new ModuleId(1), 'submodule_1');
+		$module = new Module(new ModuleId(1), 'module_1');
 
-		$first_mock = $this->createMock(ISubmoduleRepository::class);
-		$first_mock->method('findById')->willReturn($this->submodule);
-		$first_mock->method('exists')->willReturn(true);
+		$this->submodule_repository = $this->createMock(ISubmoduleRepository::class);
+		$this->submodule_repository->method('findById')->willReturn($submodule);
+		$this->submodule_repository->method('exists')->willReturn(true);
 
-		$second_mock = $this->createMock(IModuleRepository::class);
-		$second_mock->method('findById')->willReturn($this->module);
-	
-		$this->submodule_service = new SubmoduleService($first_mock, $second_mock); 
-	
+		$this->module_repository = $this->createMock(IModuleRepository::class);
+		$this->module_repository->method('findById')->willReturn($module);
+
+		$this->submodule_repository_null = $this->createMock(ISubmoduleRepository::class);
+		$this->submodule_repository_null->method('findById')->willReturn(null);
+
+		$this->module_repository_null = $this->createMock(IModuleRepository::class);
+		$this->module_repository_null->method('findById')->willReturn(null);
+		
 	}
 
 
 	public function testIfGetByIdReturnsCorrectSubmodule() {
+
+		$this->submodule_service = new SubmoduleService($this->submodule_repository, $this->module_repository); 
 
 		$check_get_id = $this->submodule_service->getById(1);
 
@@ -50,6 +57,8 @@ class SubmoduleServiceTest extends TestCase {
 
 	public function testIfGetParentModuleReturnsModule() {
 
+		$this->submodule_service = new SubmoduleService($this->submodule_repository, $this->module_repository); 
+
 		$parent_module = $this->submodule_service->getParentModule(1);
 
 		$return_parent = $parent_module->id();
@@ -60,11 +69,31 @@ class SubmoduleServiceTest extends TestCase {
 
 	public function testIfExistsReturnsExistingSubmodule() {
 
+		$this->submodule_service = new SubmoduleService($this->submodule_repository, $this->module_repository); 
 
 		$check_submodule = $this->submodule_service->exists(1);
 
 		$this->assertTrue($check_submodule);
 
+	}
+
+	public function test_getParentModule_Throws_Exception_If_Submodule_Isnt_Found(){
+
+		$this->expectException(SubmoduleNotFoundException::class);
+
+		$this->submodule_service = new SubmoduleService($this->submodule_repository_null, $this->module_repository); 
+
+		$this->submodule_service->getParentModule(1);
+
+	}
+
+	public function test_getParentModule_Throws_Exception_If_Module_Isnt_Found(){
+
+		$this->expectException(ModuleNotFoundException::class);
+
+		$this->submodule_service = new SubmoduleService($this->submodule_repository, $this->module_repository_null);
+
+		$this->submodule_service->getParentModule(1);
 	}
 
 }
